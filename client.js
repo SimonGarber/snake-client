d807bba4e8f9ca884d5c17fbed60463c12d8b986
Simon Garber <Simon.garber@gmail.com>
2019-07-03 15:18:55 -0400

    refactored the code to export the module to the client.js file and we are now calling the connect function inside es6 object notation.

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '172.46.0.110',
+    port: 50541
+    
+  });
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  // interpret incoming data as text
+}) 
+
+  return conn;
+}
+
+
+
+
+
+module.exports = {connect};
